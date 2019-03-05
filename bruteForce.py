584433e829e0e08feea0125b07b9f5861fbfbf57
Ben Reilly <ben8699@gmail.com>
2019-03-05 20:46:49 +0000

    Complete recode.

--- bruteForce.py
+++ bruteForce.py
@@ -2,98 +2,83 @@ import sys
 from itertools import product
 import time
 
-def full(): # Max 3 chars
-    maxchars = str(input("Enter max number of characters to be tried: "))
-    intmaxchars = int(maxchars)
-    user_password = input("Enter a "+ maxchars +" character password: ").upper()
-    found = False
+def init():
+    global passwordAlphabet, firstTime
+    passwordAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$* "
+    firstTime = True
+    firstTimeMenu()
+
+def crack():
+    found = False 
     BFCounter = 0
-    passwordAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    intmaxchars += 1
-
-    for length in range(1, intmaxchars):
+    BFclearCounter = 0
+    
+    for length in range(1, 10):
         password_to_attempt = product(passwordAlphabet, repeat=length)
+                                     
+        for attempt in password_to_attempt:
+            attempt = ''.join(attempt)
+            BFCounter += 1
+            BFclearCounter += 1
 
-    for attempt in password_to_attempt:
-        attempt = ''.join(attempt)
-        BFCounter += 1
-        print("Attempt Number:", BFCounter, "with attempt of", attempt)
+            if BFclearCounter > 4999999:
+                print("Brute force in progress...          Attempt #", BFCounter)
+                BFclearCounter = 0
 
-        if attempt == user_password:
-            print("Your password is:", attempt,"and was found in", BFCounter,"attempts!")
-            found = True
-            break
-
+            if attempt == user_password:
+                print("The password is "+ attempt +"! It was found in" , BFCounter, "attempts!")
+                
+                found = True
+                break
         if found:
             break
+    if found:
+        firstTime = False
+        menu()
 
-def mid(): # Max 5 chars
-    maxchars = str(input("Enter max number of characters to be tried: "))
-    intmaxchars = int(maxchars)
-    user_password = input("Enter a "+ maxchars +" character password: ").upper()
-    found = False
-    BFCounter = 0
-    BFCounterClear = 0
-    passwordAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    intmaxchars += 1
+def firstTimeMenu():
+    print("************Welcome to Password Cracker**************")
+    print()
+    choice = input("""
+        A: Run the Password Cracker
+        B: Quit Program
 
-    for length in range(1, intmaxchars):
-        password_to_attempt = product(passwordAlphabet, repeat=length)
+        Please enter your choice: """)
 
-    for attempt in password_to_attempt:
-        attempt = ''.join(attempt)
-        BFCounter += 1
-        BFCounterClear += 1
-
-        if BFCounter < 100001 and BFCounterClear == 1000:
-            print("Attempt number:", BFCounter,"with attempt of", attempt)
-            BFCounterClear = 0
-            
-        if BFCounter > 100000 and BFCounter < 1000001 and BFCounterClear == 10000:
-            print("Attempt number:", BFCounter,"with attempt of", attempt)
-            BFCounterClear = 0
-
-        if BFCounter > 1000001 and BFCounterClear == 100000:
-            print("Attempt number:", BFCounter,"with attempt of", attempt)
-            BFCounterClear = 0
         
-        if attempt == user_password:
-            print("Your password is:", attempt,"and was found in", BFCounter,"attempts!")
-            found = True
-            break
+    if choice == "A" or choice =="a":
+        usrPass()
+    elif choice == "B" or choice =="b":
+        sys.exit
+    else:
+        print("You must only select either A or B")
+        print("Please try again")
+        menu()
 
-        if found:
-            break
+def menu():
+    print()
+    print()
+    print()       
+    print("************Welcome to Password Cracker**************")
+    choice = input("""
+        A: Run again
+        B: Quit Program
 
-def low():
-    maxchars = str(input("Enter max number of characters to be tried: "))
-    intmaxchars = int(maxchars)
-    user_password = input("Enter a "+ maxchars +" character password: ").upper()
-    found = False
-    print("Starting to Brute force...")
-    BFCounter = 0
-    passwordAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    intmaxchars += 1
+        Please enter your choice: """).strip()
 
-    for length in range(1, intmaxchars):
-        password_to_attempt = product(passwordAlphabet, repeat=length)
+        
+    if choice == "A" or choice =="a":
+        usrPass()
+    elif choice == "B" or choice =="b":
+        sys.exit
+    else:
+        print("You must only select either A or B")
+        print("Please try again")
+        menu()
 
-    for attempt in password_to_attempt:
-        attempt = ''.join(attempt)
-        BFCounter += 1
+def usrPass():
+    global user_password
+    user_password = input("Enter a password: ").upper()
+    crack()
 
-        if attempt == user_password:
-            print("Your password is:", attempt,"and was found in", BFCounter,"attempts!")
-            found = True
-            break
-
-        if found:
-            break
-
-menu = int(input("Enter which mode you want, 1) Full 2) Mid 3) Low : "))
-if menu == 1:
-    full()
-if menu == 2:
-    mid()
-if menu == 3:
-    low()
+init()
